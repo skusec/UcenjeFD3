9935c4ec8951ff70d2c138d3c96bfc8a0a6bcfd7
Sean Kušec <s.kusec88@gmail.com>
2026-07-13 18:55:44 +0200

    Uvod u JavaScript

---
+++ 03_JS/vjezba01/skripta.js
@@ -0,0 +1,21 @@
+
+// alert('Hello world 1')
+
+
+document.getElementById('ispis').innerText = 'Hello world 6'
+
+document.getElementById('ispis').style.color='red'
+
+document.getElementById('ge').innerHTML=
+'<a href="#">Ovo je poveznica</a>'
+
+
+// Backticks
+
+console.log(`
+    
+    Ovaj tekst mogu 
+    pisati u više redova
+    ...kasnije druge pogodnosti
+    
+    `)
