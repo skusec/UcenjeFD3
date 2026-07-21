b5efe6ac95271f42f5b8805afa87e47f05baca6c
Sean Kušec <s.kusec88@gmail.com>
2026-07-21 15:39:03 +0200

    Vježbanje if (vjezba05 folder) 3/3 zadatka

--- 03_JS/vjezba05/zad/skripta.js
+++ 03_JS/vjezba05/zad/skripta.js
@@ -21,21 +21,98 @@ document.getElementById('izvedi').addEventListener('click', () => {
 
         if (!a) {   //ako nisi broj (ili si 0, NaN, null ili undefined)
             rezultat.innerHTML = 'A nije broj'
-            rezultat.style.color='red'
+            rezultat.style.color= 'red'
             return //prekida izvođenja click funkcije - short curcuiting
         }
 
         const b = Number(bString)
         if (!b) {
             rezultat.innerHTML = 'B nije broj'
-            rezultat.style.color='red'
+            rezultat.style.color= 'red'
             return
         }
+        
+        //kao program sam ovdje siguran da su a i b brojevi
+        rezultat.innerHTML = a > b ? a : b
+        rezultat.style.color = 'green'
 
-
-
-
+        //kraj 1.zadatka
 
     }
+
+    if(zadatak === '2'){
+    //početak 2.zadatka
+    
+    const a = Number(aString)
+    if(!a){
+        rezultat.innerHTML = 'A nije broj'
+        rezultat.style.color = 'red'
+        return
+    }
+
+    const b = Number(bString)
+    if(!b){
+        rezultat.innerHTML = 'B nije broj'
+        rezultat.style.color = 'red'
+        return
+    }
+
+    const c = Number(cString)
+    if(!c){
+        rezultat.innerHTML = 'C nije broj'
+        rezultat.style.color = 'red'
+        return
+    }
+
+    const veceIzmeduAiB = a > b ? a : b
+
+    rezultat.innerHTML = veceIzmeduAiB > c ? veceIzmeduAiB : c
+    rezultat.style.color = 'green'
+
+    //kraj 2.zadatka
+
+    }
+
+    if(zadatak === '3'){
+    //početak 3.zadatka
+
+    const a = Number(aString)
+    if(!a){
+        rezultat.innerHTML = 'A nije broj'
+        rezultat.style.color = 'red'
+        return
+    }
+
+    const b = Number(bString)
+    if(!b){
+        rezultat.innerHTML = 'B nije broj'
+        rezultat.style.color = 'red'
+        return
+    }
+
+    const c = Number(cString)
+    if(!c){
+        rezultat.innerHTML = 'C nije broj'
+        rezultat.style.color = 'red'
+        return
+    }
+
+    const d = Number(dString)
+    if(!d){
+        rezultat.innerHTML = 'D nije broj'
+        rezultat.style.color = 'red'
+        return
+    }
+
+    const manjiIzmeduAiB = a < b ? a : b
+    const manjiIzmeduCiD = c < d ? c : d
+
+    rezultat.innerHTML = manjiIzmeduAiB < manjiIzmeduCiD ? manjiIzmeduAiB : manjiIzmeduCiD
+    rezultat.style.color = 'green'
+
+    //kraj 3.zadatka
+    
+    }
+
 
 })
